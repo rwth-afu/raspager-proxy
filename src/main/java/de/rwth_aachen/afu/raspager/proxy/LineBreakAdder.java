9c12775fca724c5c91c017e967435c03cb670812
Philipp Thiel <pthiel@radicalworlds.de>
2017-02-02 18:53:02 +0100

    Fixed missing sharable annotation.

--- src/main/java/de/rwth_aachen/afu/raspager/proxy/LineBreakAdder.java
+++ src/main/java/de/rwth_aachen/afu/raspager/proxy/LineBreakAdder.java
@@ -18,6 +18,7 @@ package de.rwth_aachen.afu.raspager.proxy;
 
 import java.util.List;
 
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
 
@@ -26,11 +27,12 @@ import io.netty.handler.codec.MessageToMessageEncoder;
  *
  * @author Philipp Thiel
  */
+@Sharable
 class LineBreakAdder extends MessageToMessageEncoder<String> {
 
-    @Override
-    protected void encode(ChannelHandlerContext ctx, String msg, List<Object> out) throws Exception {
-        out.add(String.format("%s\r\n", msg));
-    }
+	@Override
+	protected void encode(ChannelHandlerContext ctx, String msg, List<Object> out) throws Exception {
+		out.add(String.format("%s\r\n", msg));
+	}
 
 }
